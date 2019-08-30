dc6fa9d7b0f59278b3ee70ec9bc4693e899c4690
alexisbellot <45399314+alexisbellot@users.noreply.github.com>
2019-08-30 14:09:14 +0100

    Update Variable Importance and tests for HBS model.R

--- Variable Importance and tests for HBS model.R
+++ Variable Importance and tests for HBS model.R
@@ -1,14 +1,11 @@
 options(java.parameters = "-Xmx5000m")
 library(bartMachine)
 library(ICEbox)
-library('xlsx')
 library(ggplot2)
 library('gtools')
 
-### Variable selection for Bayesian Non-Linear Survival Model
+### Variable selection for Hierarchical Bayesian Survival Model
 
-### Need bartmachine package
-### Same bart settings as Survival model
 
 ### --------------------------------------------------------------------------------------
 
@@ -38,19 +35,7 @@ feature_selection = function(surv.output){
   bart = bartMachine(X=X[1:500,], y=y, verbose=FALSE)
   return(var_selection_by_permute(bart))
 }
-feat_imp = feature_selection(ggITE2)
-imp = data.frame(Observed = feat_imp$var_true_props_avg, Null = apply(feat_imp$permute_mat,2,function(x)quantile(x,probs=0.90)),
-                 Names = names(feat_imp$var_true_props_avg))
-imp$Names = factor(imp$Names, levels=imp$Names)
-ggplot(data=imp[1:17,]) + geom_bar(aes(x=Names,y=Observed,colour="Observed"),stat='identity',fill='white') + 
-  geom_point(aes(x=Names,y=Null,colour='Null'))+
-  scale_colour_manual(name="",values=c('Observed'="black", 'Null'="red"))+
-  theme(legend.title = element_blank(),legend.position = c(0.9,0.9),
-        axis.text=element_text(size=12),
-        panel.grid.major = element_line(colour = "gray90"),
-        panel.grid.minor = element_line(colour = "gray90"),
-        panel.background = element_rect(fill='white'),
-        axis.text.x = element_text(angle = 40, hjust = 1))+labs(x = "", y="Inclusion Proportions")
+
 
 
 ### --------------------------------------------------------------------------------------
@@ -93,7 +78,7 @@ observed_effect = function(){
   surv.ice = ice(object = bart,X=X,y=y,predictor="Diabetes",verbose=FALSE)
   return(surv.ice$ice_curves[,2]-surv.ice$ice_curves[,1])
 }
-rank_test = function(n){
+null_effects = function(n){
   
   null_effects <- matrix(nrow=500,ncol=n)
   for (i in 1:n){
@@ -107,26 +92,17 @@ rank_test = function(n){
   }
   return(null_effects)
 }
-observed = observed_effect()
-null = rank_test(500)
 
-pval = numeric(500)
-for (i in 1:500){
-  pval[i] = mean(abs(observed[i]) <= abs(null[i,])) # p value
+permutation_test = function(n_permutations = 500){
+  
+  observed = observed_effect()
+  null = rank_test(n_permutations)
+
+  pval = numeric(n_permutations)
+  for (i in 1:n_permutations){
+    pval[i] = mean(abs(observed[i]) <= abs(null[i,])) # p value
+  }
+  return(pval)
 }
 
-# average reduction in median survival times for diabetic patients related to diabetes is
-exp(mean(observed))-1
 
-mean(pval[which(dat$Diabetes[1:500]==1)]<0.05)
-which(pval[which(dat$Diabetes[1:500]==1)]<0.05)
-mean(dat$Weight[which(pval[which(dat$Diabetes[1:500]==1)]<0.05)])
-mean(dat$Weight[which(pval[which(dat$Diabetes[1:500]==1)]>=0.05)])
-mean(dat$Body.Mass.Index[which(pval[which(dat$Diabetes[1:500]==1)]<0.05)])
-mean(dat$Body.Mass.Index[which(pval[which(dat$Diabetes[1:500]==1)]>=0.05)])
-
-
-#test42 = data.frame(null=exp(null42$x)-1)
-#test43 = data.frame(null=exp(null43$x)-1)
-write.csv(null, 'LVAD_null.csv')
-write.csv(truth, 'LVAD_truth.csv')
